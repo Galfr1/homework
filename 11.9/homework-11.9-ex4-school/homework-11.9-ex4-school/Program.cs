0ac855f5ed1f4d94cfce45a2cd6a7b561fa80b9b
Gal Fried <58070453+Galfr1@users.noreply.github.com>
2022-09-12 18:25:12 +0300

    Add files via upload

---
+++ 11.9/homework-11.9-ex4-school/homework-11.9-ex4-school/Program.cs
@@ -0,0 +1,60 @@
+﻿//Made by Gal Fried
+
+//This code is in .NET 6 and isn't compatible with older versions
+//see https://aka.ms/new-console-template for more information
+
+//printing instructions to the user
+Console.WriteLine("This program will calculate stuff about swiming.");
+Console.WriteLine("Please enter the length of the pool in meters:");
+
+//initializing pool length double
+double poolLength = 100;
+
+try
+{
+    //trying to read input from the user and saving it to the varrible
+    poolLength = double.Parse(Console.ReadLine());
+}
+catch
+{
+    //if there is an eror, printing out to the user
+    Console.WriteLine("You didn't enter a valid length, using the default 100.");
+}
+
+//printing instructions to the user
+Console.WriteLine("Enter how many pools you swim per workout:");
+
+//initializing double for the amount of pools
+double pools = 100;
+
+try
+{
+    //trying to read input to the user and saving it to the varrible
+    pools = double.Parse(Console.ReadLine());
+}
+catch
+{
+    //if there is an eror, printing out to the user
+    Console.WriteLine("You didn't enter a valid amount of pools, using the default 100.");
+}
+
+//printing instructions to the user
+Console.WriteLine("Enter how many workouts you do per month:");
+
+//initializing amount of workouts int
+int workouts = 10;
+
+try
+{
+    //trying to read input from the user and saving it to the varrible
+    workouts = int.Parse(Console.ReadLine());
+}
+catch
+{
+    //if there is an eror, printing out to the user
+    Console.WriteLine("You didn't enter a valid amount of workouts, using the default 10.");
+}
+
+//printing the result
+Console.WriteLine("You swim " + (workouts * pools * poolLength) + "m per month.");
+
---
+++ 11.9/homework-11.9-ex5-school/homework-11.9-ex5-school/Program.cs
@@ -0,0 +1,60 @@
+﻿//Made by Gal Fried
+
+//This code is in .NET 6 and isn't compatible in older versions
+//see https://aka.ms/new-console-template for more information
+
+//printing instructions to the user
+Console.WriteLine("Welcome to salery calculator!");
+Console.WriteLine("Please enter the base salery:");
+
+//initializing base salery double
+double baseSalery = 5000;
+
+try
+{
+    //trying to read input from the user and saving it to the varrible
+    baseSalery = double.Parse(Console.ReadLine());
+}
+catch
+{
+    //if there is an eror, printing out to the user
+    Console.WriteLine("You didn't enter a valid base salery, using the default 5000.");
+}
+
+//printing instructions to the user
+Console.WriteLine("Please enter fee (for no fee, leave empty):");
+
+//initializing fee double
+double fee = 0;
+
+try
+{
+    //trying to read input from the user and saving it to the varrible
+    fee = double.Parse(Console.ReadLine());
+}
+catch
+{
+    //if there is an eror, printing out to the user
+    Console.WriteLine("You didn't enter a valid fee, using no fee.");
+}
+
+//printing instructions to the user
+Console.WriteLine("Please enter the amount of times you want to give fee (for no fee, leave empty):");
+
+//initializing int for how many times to give the fee
+int feeAmount = 0;
+
+try
+{
+    //trying to read input from the user and saving it to the varrible
+    feeAmount = int.Parse(Console.ReadLine());
+}
+catch
+{
+    //if there is an eror, printing out to the user
+    Console.WriteLine("You didn't enter a valid number for how many times you want to give fee, using no fee");
+}
+
+//printing the result
+Console.WriteLine("The total salery will be " + (baseSalery + (fee * feeAmount)) + "$");
+
